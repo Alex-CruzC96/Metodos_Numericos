24330d9f9b071c004abb9009fcd1e38efd414ed1
Alejandro Cruz <alexcruz_c96@outlook.com>
2023-10-13 19:34:45 -0600

    Metodo de interpolación de newton

---
+++ interpolacion.py
@@ -0,0 +1,24 @@
+import sympy as sp
+
+def newton_interpolation(x_data, y_data):
+    n = len(y_data)
+    a=[]
+    x = sp.symbols('x')
+    f = sp.sympify(y_data)
+    for i in range(n):
+        for j in range(n-1, i, -1):
+            f[j] = (f[j] - f[j-1]) / (x_data[j] - x_data[j-i-1])
+        a.append(f[i])
+    for i in a:
+        print(f"Los valores de a son: {i}")
+    p = a[n-1]
+    for k in range(n-2, -1, -1):
+        p = a[k] + (x - x_data[k])*p
+    return sp.expand(p)
+
+x_data=[1,0,-3]
+y_data=[2,4,-2]
+
+p = newton_interpolation(x_data, y_data)
+print(f"El polinomio interpolante es {p}")
+
