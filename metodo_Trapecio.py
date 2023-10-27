b49f19243edab9d96985e9e171d0edd8fda5782b
Alejandro Cruz <alexcruz_c96@outlook.com>
2023-10-27 11:05:58 -0600

    Método de Trapecio

---
+++ metodo_Trapecio.py
@@ -0,0 +1,40 @@
+import sympy as sp
+from sympy.plotting import plot
+import matplotlib.pyplot as plt
+import numpy as np
+
+X=sp.Symbol("x")
+while(True):
+    try:
+        func=input("Ingrese la función en términos de X: ")
+        function=sp.sympify(func)
+        derivada=sp.diff(function,X)
+        derivada2=sp.diff(derivada,X)
+        break
+    except ValueError:
+        pass
+
+f=sp.lambdify(X,function)
+f2=sp.lambdify(X,derivada2)
+a=float(input("Ingrese el límite inferior: "))
+b=float(input("Ingrese el límite superior: "))
+media=(b-a)/2
+eval1=f(a)
+eval2=f(b)
+error=(-1*(1/12))*(f2((b-a)))*(b-a)**3
+resultado=(media*(f(a)+f(b)))+error
+print(f"El resultado es: {resultado}")
+
+x_val=np.linspace(a,b,400)
+y_val=f(x_val)
+
+plt.figure(figsize=(8,6))
+plt.plot(x_val,y_val,label=f"Funcion = {function}")
+plt.scatter([a,b],[f(a),f(b)],color='red')
+plt.fill_between(x_val,y_val,color='lightblue',alpha=0.5,label=f"Área encontrada = {resultado}")
+plt.title('Grafica')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+plt.legend()
+plt.show()
